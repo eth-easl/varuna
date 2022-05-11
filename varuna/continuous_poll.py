643183de7dd5878a89a66729e27b6c2a03aaa8a9
fotstrt <fotstrt95@gmail.com>
2022-05-11 20:31:47 +0200

    take input from simulator

--- varuna/continuous_poll.py
+++ varuna/continuous_poll.py
@@ -5,19 +5,24 @@ import time
 import sys
 from random import randint
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Poll for updates and notify')
+
+parser.add_argument('--server-ip', default="127.0.0.1", type=str, help='IP address of the morph server')
+parser.add_argument('--server-port', default=1234, type=int, help='Port of the morph server')
+parser.add_argument('--from-sim', default=False, action='store_true', help='Whether or not to get available VMs from simulator')
+parser.add_argument('--zone', default="us-west1-a", type=str, help='Cluster zone')
+parser.add_argument('--available_machines_list', default="", type=str, help='File of the available machine IPs')
+parser.add_argument('--running_machines_list', default="", type=str, help='File of the currently running machine IPs')
+parser.add_argument('--simulated_machines_list', default="", type=str, help='File of the simulated machine IPs')
 
 # cluster = sys.argv[1]
 counter = 0
 possibly_dead_nodes = []
 
-available_machines_list = sys.argv[1]
-running_machines_list = sys.argv[2]
-
-server_ip = sys.argv[3]
-server_port = int(sys.argv[4])
-
 pattern = 'varuna-worker'
-zone = 'europe-west4-a'
+args = parser.parse_args()
 
 def process_response(res, pattern):
 
@@ -67,7 +72,11 @@ def poll_and_update():
     current_num_machines = len(current_machines)
     print("Current:", current_machines,flush=True)
 
-    new_machines = get_available_machines()
+    new_machines = []
+    if args.from_sim:
+        new_machines = get_available_machines_sim()
+    else:
+        new_machines = get_available_machines()
     print("New", new_machines, flush=True)
 
     if sorted(new_machines) == sorted(current_machines):
@@ -75,36 +84,44 @@ def poll_and_update():
     else:
         # machines_added = [m for m in new_machines if m not in current_machines]
         msg = f"morph {len(new_machines)}"
-        client(server_ip, server_port, msg)
+        client(args.server_ip, args.server_port, msg)
         print(len(new_machines), flush=True)
 
 
 def get_current_machines():
-    f = open(running_machines_list,"r")
+    f = open(args.running_machines_list,"r")
     machines = f.read().split("\n")
     machines = [m for m in machines if len(m) > 0]
     return machines
 
-def get_available_machines():
-    vm_list = get_vm_list(pattern, zone)
-    print(vm_list)
-
+def get_avail(vm_list):
     avail_vm_list = []
 
     for x in vm_list:
-        if check_VM_running(x, zone):
-            cmd =  "gcloud compute instances describe " + x + " --format get(networkInterfaces[0].networkIP) --zone " + zone
+        if check_VM_running(x, args.zone):
+            cmd =  "gcloud compute instances describe " + x + " --format get(networkInterfaces[0].networkIP) --zone " + args.zone
             ip_addr = subprocess.run(cmd.split(), stdout=subprocess.PIPE).stdout.decode()
             print(ip_addr)
             avail_vm_list.append(ip_addr)
 
     print(avail_vm_list)
-    f = open(available_machines_list, 'w')
+    f = open(args.available_machines_list, 'w')
     for v in avail_vm_list: # TODO: what if someone is reading from this file?
         f.write(v)
     f.flush()
-
     return avail_vm_list
+
+
+def get_available_machines_sim():
+    f = open(args.simulated_machines_list,"r")
+    machines = f.read().split("\n")
+    machines = [m for m in machines if len(m) > 0]
+    return get_avail(machines)
+
+def get_available_machines():
+    vm_list = get_vm_list(pattern, args.zone)
+    print(vm_list)
+    return get_avail(vm_list)
 
 def notify():
      while True:
